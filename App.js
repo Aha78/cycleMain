a0455446e46959a63e765d16e233a4b3e670eb00
Antto <antto.hautamaki@elisanet.fi>
2023-05-11 12:15:07 +0300

    added flatllist template. to stationlist

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View,FlatList } from "react-native";
 import { Router, Route, Link } from "./react-router";
 
-const Home = () => <Text>Home</Text>;
-const Antto = () => <Text>Antto</Text>;
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+const Home = () =>
+    <View style={styles.container}>
+    <FlatList
+        data={[
+            { key: 'Android' }, { key: 'iOS' }, { key: 'Java' }, { key: 'Swift' }
+         
+        ]}
+        renderItem={({ item }) =>
+                <Link to="/stationDetails"><Text style={styles.item}>{item.key}</Text></Link>}
+        ItemSeparatorComponent={this.renderSeparator}
+    />
+</View>;
+const StationDetails = () =>
+    <View style={styles.container}>
+        <Text>StationDetails</Text>
+        <Text>Station name</Text>
+        <Text>Total number of debarting journeys</Text>
+        <Text>Total number of return journeys</Text>
+        <Text>Map</Text>
+    </View>;
+
 const About = () => <Text>A</Text>;
 const Test = () => <Text>B</Text>;
 const App = () => (
@@ -12,14 +45,15 @@ const App = () => (
         <Link to="/">
           <Text>Stationlist</Text>
         </Link>
-                <Link to="/test">
-                    <Text>Stationdetails</Text>
+       <Link to="/stationDetails">
+           <Text>Stationdetails</Text>
         </Link>
       </View>
 
       <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/test" component={Test} />
+            <Route path="/stationDetails" component={StationDetails} />
     </View>
   </Router>
 );
