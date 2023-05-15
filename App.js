d8d3945ba65c012687e076b2f77ef023e7fea18f
Antto <antto.hautamaki@elisanet.fi>
2023-05-15 17:58:06 +0300

    First version of working flatlist

--- App.js
+++ App.js
@@ -1,80 +1,97 @@
 import { StyleSheet, Text, View,FlatList,Button ,TouchableHighlight} from "react-native";
 import { Router, Route, Link } from "./react-router";
 import { useLocation } from 'react-router-dom'
-
+import axios from 'axios';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { useState } from "react"; 
 import { Icon } from 'react-native-elements'
 
 //demo, which I use as template to app
 // https://reactnative.dev/docs/navigation
 const Stack = createNativeStackNavigator();
 
-const DATA = [
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'First Item',
-    },
-    {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Second Item',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Third Item',
-    },
-  ];
-  
 
-  
+
+
+
 
 
 const MyStack = () => {
+
     return (
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen
                     name="Home"
                     component={Home}
-                    options={{ title: 'Welcome' }}
+                    options={{ title: 'Stationlist' }}
                 />
                 <Stack.Screen name="StationDetails" component={StadionDetails} />
             </Stack.Navigator>
         </NavigationContainer>
     );
 };
-const Home = ({ navigation }) => {
-    return (
+
+const Home =  ({ navigation }) => {
+    const [advice, Setadvice] = useState(null);
+    axios.get("http://192.168.1.3:7077/api/Stations?page=1")
+
+        .then(resp => resp.data)
+        .then(resp => Setadvice(resp))
+        
+        .catch(resp=>console.log(resp));
+
+    state = {
+        Data: 'This is a text component, created using state data. It will change or updated on clicking it.'
+    }  
+
+ 
+
+    if (advice != null) {
+        return (
+            <View>
+                <FlatList
+                    data={advice}
+                    renderItem={({ item }) => <TouchableHighlight onPress={() => alert('jes')}><Text >{item.Name}</Text></TouchableHighlight>}
+                    keyExtractor={item => item.id}
+                />
+                <View style={{ flexGrow: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                
+
+                    <TouchableHighlight onPress={() => alert('jes')}>
+                        <Icon
+                            name='arrow-left' size={80} />
+                    </TouchableHighlight>
+                    <Text>1</Text>
+                    <TouchableHighlight onPress={() => alert('jes1')} >
+                        <Icon
+                            name='arrow-right' size={80} />
+                    </TouchableHighlight>
+                </View>
+
+                <Button
+                    title="Stations details"
+                    onPress={() =>
+                        navigation.navigate('StationDetails', { name: '1' })
+                    }
+                />
+
+            </View>
+
+        );
+
+    }
+    else return (
         <View>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) =><Text >{item.title}</Text>}
-        keyExtractor={item => item.id}
-      />
- <View style={{flexGrow: 1,  flexDirection: 'row', justifyContent:'center', alignItems: 'center'}}>
-<TouchableHighlight  onPress={()=>alert('jes')}>
-<Icon
-  name='arrow-left'size={80} />
-  </TouchableHighlight>
-  <Text>3</Text>
-  <TouchableHighlight onPress={()=>alert('jes1')} >
-  <Icon
-  name='arrow-right' size={80} />
-  </TouchableHighlight>
-</View>
-   
-            <Button
-            title="Stations details"
-            onPress={() =>
-                navigation.navigate('StationDetails', { name: '1' })
-            }
-        />
 
         </View>
-  
+
     );
+
+
+
 };
 const StadionDetails = ({ navigation, route }) => {
 
